f5d656379d2db95101d6999315e6f7af5168c8e0
Jyoti Ranjan Panda <jrpanda.dev@gmail.com>
2026-09-21 01:20:33 +0530

    Prepare VayuNet Render deployment

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://vayunet-52a15.web.app",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
